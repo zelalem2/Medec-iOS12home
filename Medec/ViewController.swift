eb9ffb7a1f9c2e0a7daf74db291e28fba773cdfa
MacOS <macos@MacOSs-Mac.local>
2020-01-02 00:33:25 -0800

    Initial Commit

---
+++ Medec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medec
+//
+//  Created by MacOS on 1/2/20.
+//  Copyright © 2020 medInnovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
